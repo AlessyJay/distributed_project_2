d0c9945a47fdfff715414acbefbc41277077ec9d
Peeranat <alessyraffaello@gmail.com>
2023-01-20 20:05:12 +0700

    First code prototype

---
+++ distributed_project_2/Client.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace distributed_project_2
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            // Connect to the server.
+            TcpClient client = new TcpClient("127.0.0.1", 3000);
+            NetworkStream network_stream = client.GetStream();
+
+            // Proceed to read the numbers that have been sent from server and calculate.
+            int sum = 0;
+            while (true)
+            {
+                byte[] Data = new byte[1024];
+                int readBytes = network_stream.Read(Data, 0, Data.Length);
+                string num = Encoding.ASCII.GetString(Data, 0, Data.Length);
+
+                if (num == "FINISHED") break;
+
+                sum += int.Parse(num);
+            };
+
+            Console.WriteLine($"Sum: {sum}");
+
+            // Send messages to the server.
+            byte[] buffer = Encoding.ASCII.GetBytes("FINISHED");
+            network_stream.Write(buffer, 0, buffer.Length);
+
+            // Close the connection.
+            network_stream.Close();
+            client.Close();
+        }
+    }
+}
---
+++ distributed_project_2/Server.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace distributed_project_2
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            // We are using TCP socket listener.
+
+            // Create a TCP socket listener to connect with local IP address.
+            TcpListener listener = new TcpListener(IPAddress.Any, 3000);
+            listener.Start();
+
+            // Waiting for client side to communicate with the server.
+            TcpClient client = listener.AcceptTcpClient();
+            NetworkStream network_stream = client.GetStream();
+
+            // Generate number and send to client
+            while (true)
+            {
+                Random rand = new Random();
+                int num = rand.Next();
+                byte[] Data = Encoding.ASCII.GetBytes(num.ToString());
+                network_stream.Write(Data, 0, Data.Length);
+
+                // Checking if the client has sent messages to the server.
+                if (network_stream.DataAvailable)
+                {
+                    byte[] buffer = new byte[client.Available];
+                    network_stream.Read(buffer, 0, buffer.Length);
+                    string message = Encoding.ASCII.GetString(buffer);
+
+                    if (message == "FINISHED") break;
+                }
+            }
+
+            // Close the connection.
+            client.Close();
+            network_stream.Close();
+        }
+    }
+}
